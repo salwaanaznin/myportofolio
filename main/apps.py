3c6987fe0a19bb7a3835b3a93d7feef8cc0ef870
salwaanaznin <salwa.alyani@ui.ac.id>
2026-09-12 21:47:26 +0700

    auto load fixture

--- main/apps.py
+++ main/apps.py
@@ -1,6 +1,15 @@
 from django.apps import AppConfig
-
+import sys
 
 class MainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'main'
+
+    def ready(self):
+        # Mencegah komando berjalan saat jalankan makemigrations/migrate
+        if 'runserver' in sys.argv or 'gunicorn' in sys.argv[0]:
+            from django.core.management import call_command
+            try:
+                call_command('loaddata', 'initial_experiences.json')
+            except Exception:
+                pass
