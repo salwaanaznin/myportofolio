add7b07582d14aef45d819c74f170be9ec34d6ed
salwaanaznin <salwa.alyani@ui.ac.id>
2026-09-12 23:37:21 +0700

    feat:1- Add education models

--- main/models.py
+++ main/models.py
@@ -24,3 +24,27 @@ class Experience(models.Model):
     @property
     def is_ongoing(self):
         return self.ended_at is None
+
+import uuid
+from django.db import models
+
+class Education(models.Model):
+    EDUCATION_CHOICES = [
+        ('Senior High School', 'Natural Science'),
+        ('Undergraduate Information System', 'Faculty of Computer Science, Universitas Indonesia'),
+    ]
+    
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    major = models.CharField(max_length=255, blank=True, null=True)
+    degree = models.CharField(max_length=20, choices=EDUCATION_CHOICES, default='S1')
+    thumbnail = models.URLField(blank=True, null=True)
+    started_at = models.DateTimeField()
+    ended_at = models.DateTimeField(blank=True, null=True)
+    def __str__(self):
+        return f"{self.title} - {self.degree}"
+    
+    @property
+    def is_ongoing(self):
+        return self.ended_at is None
